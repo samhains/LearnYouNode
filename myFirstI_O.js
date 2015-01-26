64ed78d8ae89c6e694cdf072c0b3d1cfec7dc343
SAM HAINS <sdh@eml.cc>
2015-01-26 18:46:11 +1100

    finished learnyounode exercises

--- myFirstI_O.js
+++ myFirstI_O.js
@@ -1,1 +1,12 @@
 var fs = require('fs');
+
+if(process.argv.length>2){
+    var buf = fs.readFileSync(process.argv[2]);
+    var str = buf.toString();
+    var subStr = str.split('\n');
+
+    console.log(subStr.length-1);
+}
+
+
+
